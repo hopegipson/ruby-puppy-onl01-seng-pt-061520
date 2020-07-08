cbe7c1676abf92f913019f119dc59cd8347a3533
Hope Gipson <gipson.hope@gmail.com>
2020-07-08 06:01:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Add your code here
+class Dog
+  @@all = []
+  attr_reader :name
+
+  
+  def initialize(name)
+    @name = name
+    self.save
+    
+  end
+  
+  def save
+    @@all << self
+  end
+  
+    def self.all
+    @@all
+  end
+  
+  def self.print_all
+  @@array.each do|item|{
+    puts item
+    }
+  end
+end
